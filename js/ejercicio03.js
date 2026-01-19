488332b3e8812e2f2071672f46eb50cbbc5f5389
Marco A. Ramirez <marco.ramirez@utxicotepec.edu.mx>
2026-01-19 10:46:43 -0600

    Practica 02 - Ejercicio 03
    update: Se finalizó el código del Ejercicio 03: Funciones

--- js/ejercicio03.js
+++ js/ejercicio03.js
@@ -106,7 +106,7 @@ console.log(`${loginStatus ? 'El usuario admin se ha logeado satisfactoriamente'
 /* A diferencia de las funciones nombradas este tipo de fuciones no suelen ser reutilizadas en el código solo se ejecutan una sola vez, dando velocidad, individalidad y privacidad a los datos utilizados en el proceso*/
 
 
-// 5. Función anónima sin parámetros
+// 5. Función anónima sin parámetros (Versión Extendida)
  isNewUser = function () {
     const hoy = new Date();
 
@@ -127,3 +127,54 @@ lastLogin= new Date("2025/12/31");
 console.log("Test 2 - Fecha de ultimo acceso es diferente a la fecha de hoy");
 console.log(`La fecha del último acceso es: ${lastLogin}`);
 console.log(`El usuario logeado es: ${isNewUser()?"Nuevo Usuario":"Usuario Antiguo"}`);
+
+// 6. Funciones Anónimas con Parámetros  (Versión Arrow o Lambda)
+
+const sumar =  (a, b) =>{
+    let resultado= a+b;
+    return resultado;
+}
+
+console.warn("6. Funciones Anónima con Parámetros")
+console.log(`El resultado de la suma de 15 + 83 es: ${sumar(15,83)}`);
+
+/* Cuando la función anónima tiene solo una línea de operación se puede usar una versión simplificada que no usa {} llaves , ni la palabra reservada (return) */
+
+const multiplicar = (a,b) => a*b;
+console.log(`El resultado de la suma de 15 + 125 es: ${multiplicar(15,125)}`);
+
+
+//7. Funciones Callback (Regreso de Llamado)
+console.warn("7. Funciones Anónimas Callback (Respuesta)")
+const recoverPassword = function(email, callback)
+{
+
+    // Generamos el código a enviar al usuario.
+    const recoveryCode= Math.floor(1000000 + Math.random()*900000)
+
+    console.log(`
+=======================================================================
+Solicitud de recuperación recibida
+Correo del usuario sollicitante: ${email}
+Generando Código de Recuperación...
+Código de Seguridad Generado: ${recoveryCode}
+Enviando el correo al usuario...
+Correo Enviado a : ${email}, con el código de seguridad: ${recoveryCode}
+=======================================================================`);
+
+
+// definiendo la respues del sistema
+const response ={
+    status: "OK",
+    message: "Código de recuperación enviado satisfactoriamente."
+};
+
+callback(response);
+};
+
+// Invocación de una función callback
+recoverPassword("marco@gmail.com", 
+    function(systemResponse){
+        console.log("Respuesta del sistema: ");
+        console.log(systemResponse.message);
+    });
